588599847da6191ccea167252cfb4e623c482de0
Gediminas Dausynas <gediminas.dausynas@gmail.com>
2024-08-29 17:30:22 +0300

    ESM and CJS support

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,22 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: './src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'es',
-    entryFileNames: '[name].js',
-  },
-  plugins: [typescript()],
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+    }
+  ],
+  plugins: [
+    resolve(),  // Resolves node_modules
+    commonjs(), // Converts CommonJS to ES6
+    typescript() // Handles TypeScript
+  ],
 };

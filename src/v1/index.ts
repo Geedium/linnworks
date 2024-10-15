9ec5505b227e6ad502e0b2cc4e41eb8e43e82966
Gediminas Dausynas <gediminas.dausynas@gmail.com>
2024-10-15 21:05:58 +0300

    Dashboards & Updated README.md

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,13 +1,16 @@
 import LinnworksBase from "../api/index";
 import { ApiOptions } from "../types";
 import { Authenticate } from "./authenticate";
+import { Dashboards } from "./dashboards";
 
 class LinnworksApi extends LinnworksBase {
     public readonly authenticate: Authenticate;
+    public readonly dashboards: Dashboards;
 
     constructor(options: ApiOptions) {
         super(options);
         this.authenticate = new Authenticate(options);
+        this.dashboards = new Dashboards(options);
     }
 }
 
